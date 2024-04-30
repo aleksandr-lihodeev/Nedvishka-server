7ea918fc166ba26a9e36b1e022a893cbc6f3b838
Aleksandr <13thjavascript@gmail.com>
2024-04-30 16:39:54 +0600

    keys delete mongoose

--- controllers/card/getOwnersCardsController.js
+++ controllers/card/getOwnersCardsController.js
@@ -2,10 +2,15 @@ import OwnersCards from "../../models/OwnersCards.js";
 
 export const getOwnersCardsController = async (req, res) => {
     try {
-        const {userId} = req.user
-        const ownersCart = await OwnersCards.find({userId}).populate('items.product')
+        const { userId } = req.user;
+        const ownersCart = await OwnersCards.find({ userId }).populate('items.product');
 
-        return res.status(200).send({ ownersCart });
+        const sanitizedOwnersCart = ownersCart.map(owner => {
+            const { __v, _id, ...rest } = owner.toObject();
+            return rest;
+        });
+
+        return res.status(200).send(...sanitizedOwnersCart);
     } catch (e) {
         res.status(500).send({ message: "Internal Server Error" });
     }
