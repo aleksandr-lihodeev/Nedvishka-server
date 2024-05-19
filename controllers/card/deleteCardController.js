76a2af2f0061a77bf46e136dc8282c3cb0a5ab4b
bekazmr <bekazmr@gmail.com>
2024-05-20 02:09:49 +0600

    delete also from card

--- controllers/card/deleteCardController.js
+++ controllers/card/deleteCardController.js
@@ -1,7 +1,8 @@
 import OwnersCards from "../../models/OwnersCards.js";
+import Card from "../../models/Card.js"
 export const deleteCardController = async (req, res) => {
   const paramsId = req.params.id
-  const {userId} = req.body
+  const {userId ,card_Id} = req.body
   try {
     console.log(userId);
     console.log(paramsId ,"paramsId deleteCardController");
@@ -9,6 +10,10 @@ export const deleteCardController = async (req, res) => {
       { userId: userId },
       { $pull: { items: { _id: paramsId } } }
     );
+    
+    const deleteFromCard = await Card.findByIdAndDelete(card_Id);
+    if (!deleteFromCard) res.status(404).send({ message: "Item not found" });
+
     console.log('Product removed:', result);
     res.status(200).send({ message: "Item deleted successfully" });
   } catch (error) {
